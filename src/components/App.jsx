fdc8a1305d833996e682f5600da8b9a66f8b8ceb
chmubarak <chmohamedmubarak@gmail.com>
2022-10-10 15:57:12 +0530

    add useref to focus input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ToDoItem from "./ToDoItem";
 
-
 function App() {
-  const [todoLists, setTodoLists] = useState([])
-  const [inputText, setInputText] = useState("")
+  const [todoLists, setTodoLists] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  });
   function handleChange(event) {
-    const newValue = event.target.value
-    setInputText(newValue)
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
   function addItem() {
-    setTodoLists(prevList => {
-      return [inputText, ...prevList]
-    })
-    setInputText("")
+    setTodoLists((prevList) => {
+      return [inputText, ...prevList];
+    });
+    setInputText("");
   }
   function deleteItem(id) {
-    setTodoLists(prevList => {
+    setTodoLists((prevList) => {
       return prevList.filter((item, index) => {
-        return index !== id
-      })
-    })
+        return index !== id;
+      });
+    });
   }
   return (
     <div className="container">
@@ -28,7 +31,9 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text"
+        <input
+          type="text"
+          ref={inputRef}
           onChange={handleChange}
           value={inputText}
         />
@@ -39,12 +44,14 @@ function App() {
       <div>
         <ul>
           {todoLists.map((list, index) => {
-            return <ToDoItem
-              key={index}
-              id={index}
-              item={list}
-              onChecked={deleteItem}
-            />
+            return (
+              <ToDoItem
+                key={index}
+                id={index}
+                item={list}
+                onChecked={deleteItem}
+              />
+            );
           })}
         </ul>
       </div>
